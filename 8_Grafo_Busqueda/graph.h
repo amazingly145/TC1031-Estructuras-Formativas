d60c09a9f1569861da982f42df9f1b0d865007c4
Ng <benjaminva@gmail.com>
2023-09-28 10:37:22 -0600

    Update graph.h

--- 8_Grafo_Busqueda/graph.h
+++ 8_Grafo_Busqueda/graph.h
@@ -5,8 +5,8 @@
 *  desc: estos programas son solciones parciales a algunos de
 *  las actividades planteadas en el curso TC1031
 *
-*
-*  Extiende este archivo para completar tu soución.
+*		Nota: Esta imlpementación es Top Down, no divide al árbol para la operacións
+*		de barrado
 */
 
 #ifndef Graph_H_
@@ -36,35 +36,53 @@ private:
 		//vector<Type> *vect = new vector<Type>
 
 public:
-		void loadGraphMat(string, int, int);
+	
+  	void loadGraphList(string, int);
+  	void loadGraphMat(string, int, int);
 		Graph(int);
 		Graph();
+		void addEdgeAdjList(int, int);
 		void addEdgeAdjMatrix(int, int);
+		string printAdjList();
 		string printAdjMat();
 		string printAdjMat_clean();
+		string DFS(int, int);
+		string depthHelper(int, int, stack<int>&, list<int>&, vector<vector<int>>&);
+		string BFS(int, int);
+		string breadthHelper(int, int, queue<int>&, list<int>&, vector<vector<int>>&);
+		string print_visited(list<int>);
+		string print_path(vector<vector<int>>&,int ,int);
 		bool contains(list<int>, int);
 		void sortAdjList();
 };
 
 
-void Graph::loadGraphMat(string name, int a, int b){
+void Graph::loadGraphList(string data, int a){
+	adjList = new vector<int>[a];
+	nodes = a;
+	int u, v, i = 0;
+	while ( i < data.size()) {
+			u = (int)data[i+1] - 48;
+			v = (int)data[i+3] - 48;
+			i = i + 6;
+			addEdgeAdjList(u,v);
+//			cout << "u  "  << u << " v " << v << endl;
+	}
+	sortAdjList();
+}
+
+void Graph::loadGraphMat(string data, int a, int b){
 	adjMatrix = new int [a*b];
 	nodes = a;
 	for (int i = 0; i < a*b; i++)
 		adjMatrix[i] = 0;
-		string line;
-		ifstream lee (name);
-		int u, v;
-		if (lee.is_open()){
-			while (getline(lee, line)){
-				u = stoi(line.substr(1,1));
-				v = stoi(line.substr(4,1));
-				addEdgeAdjMatrix(u,v);
-			}
-			lee.close(); // Closes the file
-		} else {
-			cout << "Unable to open file";
-		}
+	int u, v, i = 0;
+	while (i < data.size()) {
+		u = (int)data[i+1] - 48;
+		v = (int)data[i+3] - 48;
+		i = i + 6;
+		addEdgeAdjMatrix(u,v);
+	}
 }
 
 Graph::Graph() {
@@ -80,55 +98,5 @@ Graph::Graph(int n) {
 	edgesList = edgesMat = 0;
 }
 
-void Graph::addEdgeAdjMatrix(int u, int v){
-	adjMatrix[u*nodes+v] = 1;
-	adjMatrix[v*nodes+u] = 1;
-	edgesMat++;
-}
-
-string Graph::printAdjList(){
-	  stringstream aux;
-		for (int i = 0; i < nodes; i++){
-	        aux << "vertex "
-	             << i << " :";
-	        for (int j = 0; j < adjList[i].size(); j ++){
-							 aux << " " << adjList[i][j];
-					}
-	        aux << " ";
-    }
-		return aux.str();
-
-}
-
-string Graph::printAdjMat(){
-	stringstream aux;
-	for (int i = 0; i < nodes; i++){
-	   for (int j = 0; j < nodes; j++){
-			 aux << adjMatrix[i*nodes+j] << " ";
-		 }
-  }
-	return aux.str();
-}
-
-string Graph::printAdjMat_clean(){
-	stringstream aux;
-	aux << "\n nodes \t|";
-	for (int i = 0; i < nodes; i++){
-			aux << "\t" << i ;
-	}
-	aux << "\n";
-	for (int i = 0; i < nodes; i++){
-			aux << "__________";
-	}
-	aux << "\n";
-	for (int i = 0; i < nodes; i++){
-		 aux << i << "\t|";
-	   for (int j = 0; j < nodes; j++){
-			 aux << "\t" << adjMatrix[i*nodes+j];
-		 }
-	   aux << "\n";
-  }
-	return aux.str();
-}
 
 #endif /* Graph_H_ */
